7c6ea4739603ddfe603b4db2092f22c418d2b2da
vishal <mgvishalctr@gmail.com>
2025-03-19 13:03:55 +0530

    push

---
+++ age_calculator/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import AgeCalculator from './components/Agecalculator';
+
+function App() {
+  return (
+    <div className="App">
+ <AgeCalculator/>
+    </div>
+  );
+}
+
+export default App;
